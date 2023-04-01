a25f0b7f1cff2f014487c78625e2a3d2cfd49cf4
cnaran <116510629+cnaran@users.noreply.github.com>
2023-04-01 20:22:46 +0200

    generate lobby access code #88

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/LobbyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/LobbyService.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.hase.soprafs23.service;
 
+import ch.uzh.ifi.hase.soprafs23.entity.Lobby;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.repository.LobbyRepository;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
@@ -24,16 +26,42 @@ public class LobbyService {
     }
 
     public void createLobby(User host){
-        //TODO
+        Lobby newLobby = new Lobby();
+        newLobby.setAccessCode(generateAccessCode());
+        addUser(host); //to-do: change method parameter => host must be added to lobby
+
+        //save new lobby to database
+        newLobby = lobbyRepository.save(newLobby);
+        lobbyRepository.flush();
+
+        log.debug("Created information for Lobby: {}", newLobby);
+        // return newLobby;
     }
+
+    //generate a random number between 10000 and 99999
     private int generateAccessCode(){
-        //TODO
-        return 0;
+        Random random = new Random();
+        int accessCode = 0;
+
+        //generate random access code until it is unique
+        boolean accessCodeIsUnique = false;
+        while (!accessCodeIsUnique){
+            //generate random number between 10000 and 99999
+            accessCode = random.nextInt(89999) + 10000;
+
+            Lobby lobbyByAccessCode = lobbyRepository.findByAccessCode(accessCode);
+            if (lobbyByAccessCode != null){
+                accessCodeIsUnique = true;
+            }
+        }
+
+        return accessCode;
     }
 
     //class diagram says that this method takes an integer, thought user fits better
     public void addUser(User user){
         //TODO
+        //verify that the user is not in another lobby already => throw error
     }
     //Accesscode in lobby is int, therefore i take int instead of String (which is written in class diagram
     //for this method
@@ -50,6 +78,5 @@ public class LobbyService {
     public void deleteLobby(int lobbyId){
         //TODO
     }
-
 
 }
