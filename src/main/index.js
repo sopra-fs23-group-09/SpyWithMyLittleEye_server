87e15cc55c43c923eda352e539dfc40437d239b9
Thereza Schiemer <athzs@Therezas-MacBook-Pro.local>
2023-04-01 16:04:37 +0200

    Added new file for websocket, tried writing initial code.

---
+++ src/main/index.js
@@ -0,0 +1,25 @@
+const { WebSocket, WebSocketServer } = require('ws');
+const http = require('http');
+
+// Spinning the http server and the WebSocket server.
+const server = http.createServer();
+const wsServer = new WebSocketServer({ server });
+const host = 'localhost';
+const port = 8100;
+server.listen(port, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+// A new client connection request received
+wsServer.on('connection', function(connection) {
+    // Generate a unique code for every user
+    const userId = uuidv4();
+    console.log('Recieved a new connection');
+
+    // Store the new connection and handle messages
+    clients[userId] = connection;
+    console.log(`${userId} connected.`);
+    connection.on('message', (message) => handleMessage(message, userId));
+    // User disconnected
+    connection.on('close', () => handleDisconnect(userId));
+});
