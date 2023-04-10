8b938c51082467b99763754d95437b7c446eec54
cnaran <116510629+cnaran@users.noreply.github.com>
2023-04-10 23:13:05 +0200

    fixed createLobby()

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/LobbyGetDTO.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.hase.soprafs23.rest.dto;
+
+public class LobbyGetDTO {
+    private int id;
+    private int accessCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccessCode() {
+        return accessCode;
+    }
+
+    public void setAccessCode(int accessCode) {
+        this.accessCode = accessCode;
+    }
+}
+
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/LobbyPostDTO.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs23.rest.dto;
+
+public class LobbyPostDTO {
+    private String amountRounds;
+
+    public String getAmountRounds() {
+        return amountRounds;
+    }
+
+    public void setAmountRounds(String amountRounds) {
+        this.amountRounds = amountRounds;
+    }
+}
