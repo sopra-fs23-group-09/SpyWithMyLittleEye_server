ba0e8ff7865a632ea53e20698926455855bf9361
Thereza Schiemer <athzs@Therezas-MBP.fritz.box>
2023-04-11 20:10:38 +0200

    Added empty function in LobbyController

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyController.java
@@ -4,15 +4,13 @@ import ch.uzh.ifi.hase.soprafs23.entity.Lobby;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 import ch.uzh.ifi.hase.soprafs23.rest.dto.LobbyGetDTO;
 import ch.uzh.ifi.hase.soprafs23.rest.dto.LobbyPostDTO;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPutDTO;
 import ch.uzh.ifi.hase.soprafs23.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs23.service.LobbyService;
 import ch.uzh.ifi.hase.soprafs23.service.UserService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LobbyController {
@@ -26,7 +24,7 @@ public class LobbyController {
     }
 
     @PostMapping("/lobbies")
-    public ResponseEntity<LobbyGetDTO> createLobby(@RequestHeader(value = "token", defaultValue = "null") String token, @RequestBody LobbyPostDTO lobbyPostDTO){
+    public ResponseEntity<LobbyGetDTO> createLobby(@RequestHeader(value = "token", defaultValue = "null") String token, @RequestBody LobbyPostDTO lobbyPostDTO) {
         userService.checkToken(token);
         User host = userService.getUser(userService.getUserID(token));
 
@@ -37,4 +35,9 @@ public class LobbyController {
         return ResponseEntity.created(null).body(DTOMapper.INSTANCE.convertLobbyToLobbyGetDTO(createdLobby));
     }
 
+    @PutMapping("/lobbies/join/{userId}")
+    public ResponseEntity<Void> joinLobby(@PathVariable(value = "userId") Long userId, @RequestBody String accessCode, @RequestHeader(value = "token", defaultValue = "null") String token) {
+        //TODO
+        return ResponseEntity.created(null).body(null);
+    }
 }
