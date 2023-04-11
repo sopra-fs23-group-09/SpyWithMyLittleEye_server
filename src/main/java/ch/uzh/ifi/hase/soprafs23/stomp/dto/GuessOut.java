e84f351455aa612c1ef9c615ac5de73ef0828481
cnaran <116510629+cnaran@users.noreply.github.com>
2023-04-11 18:30:01 +0200

    websocket game: keyword/color and guesses 2

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/stomp/dto/GuessOut.java
@@ -0,0 +1,20 @@
+package ch.uzh.ifi.hase.soprafs23.stomp.dto;
+
+public class GuessOut {
+
+    private String username;
+    private String guess;
+
+    public GuessOut(String username, String guess){
+        this.username = username;
+        this.guess = guess;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
