15fe4bd5c9c8c87c522e21ebe24e95ad70f19320
cnaran <116510629+cnaran@users.noreply.github.com>
2023-04-23 15:26:05 +0200

    ws spiedObject incl startTime/duration

--- src/main/java/ch/uzh/ifi/hase/soprafs23/stomp/dto/SpiedObjectOut.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/stomp/dto/SpiedObjectOut.java
@@ -3,11 +3,15 @@ package ch.uzh.ifi.hase.soprafs23.stomp.dto;
 public class SpiedObjectOut {
     private Location location;
     private String color;
+    private String startTime;
+    private int duration;
 
 
-    public SpiedObjectOut(Location location,String color){
+    public SpiedObjectOut(Location location,String color, String startTime, int duration){
         this.location = location;
         this.color = color;
+        this.startTime = startTime;
+        this.duration = duration;
     }
 
     public String getColor() {
@@ -16,5 +20,13 @@ public class SpiedObjectOut {
 
     public Location getLocation() {
         return location;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public int getDuration() {
+        return duration;
     }
 }
