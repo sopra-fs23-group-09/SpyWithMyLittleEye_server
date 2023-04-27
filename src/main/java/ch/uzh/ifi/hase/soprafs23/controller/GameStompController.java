ebc163f902f060325506fec71a363d8a04812ffe
Nina Emmermann <emmnin.ne@gmail.com>
2023-04-27 12:58:49 +0200

    Deleting Lobby after game ends

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GameStompController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GameStompController.java
@@ -104,6 +104,7 @@ public class GameStompController {
     @MessageMapping("/games/{gameId}/gameOver")
     public void endGame(@DestinationVariable("gameId") int gameId){
         gameService.handleGameOver(gameId);
+        lobbyService.deleteLobby(gameId);
         webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/gameOver", new EndRoundMessage("endGame", 0, 0));
     }
 }
