837e943f3cf0a98aeeffc3343a9d59706a6d2f66
Dillon Elste <delste@student.ethz.ch>
2023-05-25 11:17:15 +0200

    Changed GameRepository implementation to support multiple threads

--- src/main/java/ch/uzh/ifi/hase/soprafs23/repository/GameRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/repository/GameRepository.java
@@ -6,42 +6,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameRepository {
-    private static Map<Integer, Game> gameRepositoryByID = new HashMap<>();
-    private static GameRepository INSTANCE = new GameRepository();
+	private Map<Integer, Game> gameRepositoryByID = new HashMap<>();
+	private static GameRepository INSTANCE = new GameRepository();
 
-    private GameRepository(){
-        gameRepositoryByID = new HashMap<>();
-    }
+	private GameRepository() {
+		gameRepositoryByID = new HashMap<>();
+	}
 
-    ////// External interface
-    public static void reset(){INSTANCE = new GameRepository();}
+	////// External interface
+	public static void reset() {
+		INSTANCE = new GameRepository();
+	}
 
-    public static void addGame(Game game){
-        gameRepositoryByID.put(game.getId(), game);
-    }
+	public static void addGame(Game game) {
+		INSTANCE.addGame_internal(game);
+	}
 
-    public static Game getGameById(int gameId){
-        return gameRepositoryByID.get(gameId);
-    }
+	public static Game getGameById(int gameId) {
+		return INSTANCE.getGameById_internal(gameId);
+	}
 
-    public static void deleteGame(int gameId){
-        gameRepositoryByID.remove(gameId);
-    }
+	public static void deleteGame(int gameId) {
+		INSTANCE.deleteGame_internal(gameId);
+	}
 
-    ////// INTERNALS
+	////// INTERNALS
 
-    private void addGame_internal(Game game){
-        gameRepositoryByID.put(game.getId(), game);
-    }
+	private void addGame_internal(Game game) {
+		gameRepositoryByID.put(game.getId(), game);
+	}
 
-    private Game getGameById_internal(int gameId) {
-        return gameRepositoryByID.get(gameId);
-    }
+	private Game getGameById_internal(int gameId) {
+		return gameRepositoryByID.get(gameId);
+	}
 
-    private void deleteGame_internal(int gameId) {
-        Game g = gameRepositoryByID.get(gameId);
-        if (g == null)
-            return;
-        gameRepositoryByID.remove(gameId);
-    }
+	private void deleteGame_internal(int gameId) {
+		Game g = gameRepositoryByID.get(gameId);
+		if (g == null)
+			return;
+		gameRepositoryByID.remove(gameId);
+	}
 }
