a15d77eac04d485bfa6b8817404567fbc429766b
Nina Emmermann <emmnin.ne@gmail.com>
2023-05-25 11:24:31 +0200

    Removed unused imports

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/WebSocketService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/WebSocketService.java
@@ -1,10 +1,8 @@
 package ch.uzh.ifi.hase.soprafs23.service;
 
-import ch.uzh.ifi.hase.soprafs23.repository.PlayerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,20 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class WebSocketService {
 
-    private final Logger log = LoggerFactory.getLogger(WebSocketService.class);
-    private final PlayerRepository playerRepository;
-    private final PlayerService playerService;
-    @Autowired
-    protected SimpMessagingTemplate simpMessagingTemplate;
+	private final Logger log = LoggerFactory.getLogger(WebSocketService.class);
 
-    public WebSocketService(PlayerRepository playerRepository,
-                            @Lazy PlayerService playerService) {
-        this.playerRepository = playerRepository;
-        this.playerService = playerService;
-    }
+	@Autowired
+	protected SimpMessagingTemplate simpMessagingTemplate;
 
-    public void sendMessageToSubscribers(String mapping, Object o) {
-        log.info("Sending {} to {}", o, mapping);
-        this.simpMessagingTemplate.convertAndSend(mapping, o);
-    }
+	public void sendMessageToSubscribers(String mapping, Object o) {
+		log.info("Sending {} to {}", o, mapping);
+		this.simpMessagingTemplate.convertAndSend(mapping, o);
+	}
 }
